a517399938e18049bd916ffa82473ffd5eeca02b
Thananji Shanmuganathan <tshanmuganat@student.unimelb.edu.au>
2019-06-24 16:19:38 +1000

    Initialization of Asp.net core 3.0 and react

---
+++ PikchaWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using PikchaWebApp.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PikchaWebApp.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<PikchaUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
--- src/components/Base/Checkbox.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-
-export default class Checkbox extends Component {
-	render() {
-		return <div />;
-	}
-}
