48ad754d8f9e047e05cadff69cf7ba327ab904a0
Andrew <andrewsabato8@gmail.com>
2019-09-17 17:15:02 +1000

    Contact Form iInitial layout done

--- PikchaWebApp/ClientApp/src/components/pages/Contact.js
+++ PikchaWebApp/ClientApp/src/components/pages/Contact.js
@@ -1,16 +1,29 @@
 import React, { Fragment } from "react";
 import { connect } from "react-redux";
 import SideBar from "../layout/Sidebar";
+import Header from "../layout/Header";
+import ContactDetails from "../layout/contact/ContactDetails";
+import ContactForm from "../layout/contact/ContactForm";
+import ContactMap from "../layout/contact/ContactMap";
 
 const Contact = ({ auth }) => {
   if (auth.loading) {
-    return <h2>Loading...</h2>;
+    return <h2 className='loading'>Loading...</h2>;
   }
 
   return (
     <Fragment>
       <SideBar />
-      <div className='main-container'>Contact</div>
+      <div className='main-container'>
+        <div className='main-content-container'>
+          <Header subtitle='Get in touch' title='Contact Us' />
+          <div id='contact-container'>
+            <ContactDetails />
+            <ContactMap />
+            <ContactForm />
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 };
